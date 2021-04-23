b8fd0ed27b4230f3f38af59635d24c9c5e6531da
周强 <z15129030062@outlook.com>
2021-04-23 22:36:01 +0800

    Third commit

--- ch13/13_26.h
+++ ch13/13_26.h
@@ -1,44 +1,34 @@
-#ifndef CP_ex13_26_h
-#define CP_ex13_26_h
+#ifndef CP_EX13_26_H
+#define CP_EX13_26_H
 
-#include <vector>
-#include <string>
-#include <initializer_list>
-#include <memory>
-#include <exception>
-#include<stdexcept>
+#include<vector>
+#include<string>
+#include<initializer_list>
+#include<memory>
+#include<exception>
+#include <stdexcept>
 using namespace std;
 
-
-class ConstStrBlobPtr;
-
-class StrBlob
-{
+class ConstStrBlobPtr; //前向声明
+class StrBlob {
 public:
 	using size_type = vector<string>::size_type;
-	friend class ConstStrBlobPtr;
-
-	ConstStrBlobPtr begin() const;
+	friend class ConstStrBlobPtr; //友元
+	ConstStrBlobPtr begin() const;//类外声明，使得函数可见
 	ConstStrBlobPtr end() const;
+	StrBlob() :data(make_shared<vector<string>>()) {} //默认构造
+	StrBlob(initializer_list<string> il) :data(make_shared<vector<string>>(il)) {}//构造函数
+	StrBlob(const StrBlob& sb) :data(make_shared<vector<string>>(*sb.data)) {}//拷贝构造函数
+	StrBlob& operator=(const StrBlob& s);//拷贝赋值运算符
 
-	StrBlob() :data(std::make_shared<vector<string>>()) {}
-	StrBlob(std::initializer_list<string> il) :data(std::make_shared<vector<string>>(il)) {}
-
-	// copy constructor
-	StrBlob(const StrBlob& sb) : data(std::make_shared<vector<string>>(*sb.data)) {}
-	// copyassignment operators
-	StrBlob& operator=(const StrBlob& sb);
-
-	size_type size() const { return data->size(); }
+	size_type size() const { return data->size(); } //成员函数
 	bool empty() const { return data->empty(); }
 
-	void push_back(const string& t) { data->push_back(t); }
-	void pop_back()
-	{
+	void push_back(const string& s) { return data->push_back(s); }
+	void pop() {
 		check(0, "pop_back on empty StrBlob");
-		data->pop_back();
-	}
 
+	}
 	std::string& front()
 	{
 		check(0, "front on empty StrBlob");
@@ -61,15 +51,14 @@ public:
 		check(0, "back on empty StrBlob");
 		return data->back();
 	}
-
 private:
-	void check(size_type i, const string& msg) const
-	{
-		if (i >= data->size()) throw std::out_of_range(msg);
+	void check(size_type i, const string& mgs) const { //声明在public内
+		if (i >= data->size())
+			throw std::out_of_range(mgs);
 	}
-
 private:
-	std::shared_ptr<vector<string>> data;
+	shared_ptr<vector<string>> data;
+
 };
 
 class ConstStrBlobPtr
@@ -77,7 +66,7 @@ class ConstStrBlobPtr
 public:
 	ConstStrBlobPtr() :curr(0) {}
 	ConstStrBlobPtr(const StrBlob& a, size_t sz = 0) :wptr(a.data), curr(sz) {} // should add const
-	bool operator!=(ConstStrBlobPtr& p) { return p.curr != curr; }
+	bool operator!=(ConstStrBlobPtr& p) { return p.curr != curr; }//一般来说，有自定义构造函数，则需要拷贝构造函数与拷贝赋值运算符
 	const string& deref() const
 	{ // return value should add const
 		auto p = check(curr, "dereference past end");
@@ -116,4 +105,4 @@ StrBlob& StrBlob::operator=(const StrBlob& sb)
 	data = std::make_shared<vector<string>>(*sb.data);
 	return *this;
 }
-#endif
+#endif // !CP_EX13_26_H
